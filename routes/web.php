858aae5e996d2951217b64f964528d368d7a1256
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-20 12:15:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/divisi/index', 'DivisiController@index');
+Route::get('/divisi/add', 'DivisiController@addNew');
+Route::post('/divisi/store', 'DivisiController@store');
+Route::get('/divisi/edit/{id}', 'DivisiController@edit');
+Route::post('/divisi/update', 'DivisiController@update');
+Route::get('/divisi/delete/{id}', 'DivisiController@delete');
+
