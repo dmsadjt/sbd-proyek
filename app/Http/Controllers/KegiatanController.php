39fbde0d3c4437536bca6802e7dfc07c8b4a523f
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-21 21:05:32 +0700

    Update KegiatanController.php

--- app/Http/Controllers/KegiatanController.php
+++ app/Http/Controllers/KegiatanController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class KegiatanController extends Controller
 {
-    //
+    public function index()
+    {
+        $kegiatan = DB::table('kegiatans')->get();
+        return view('kegiatan.index', ['kegiatan' => $kegiatan]);
+    }
+    public function addNew()
+    {
+        return view('kegiatan.add');
+    }
+    public function store()
+    {
+        $data = request()->validate([
+            'noDivisi' => 'required',
+            'namaDivisi' => 'required',
+            'kepalaDivisi' => 'required'
+        ]);
+
+        if (auth()->user()) {
+            DB::table('divisis')->insert($data);
+            return redirect('divisi/index');
+        } else return redirect('/login');
+    }
+
+    public function edit($noDivisi)
+    {
+        $divisi = DB::table('divisis')->where('noDivisi', $noDivisi)->first();
+        return view('divisi.edit', ['divisi' => $divisi]);
+    }
+
+    public function update(Request $request)
+    {
+        if (auth()->user()) {
+            DB::table('divisis')->where('noDivisi', $request->noDivisi)->update([
+                'noDivisi' => $request->noDivisi,
+                'namaDivisi' => $request->namaDivisi,
+                'kepalaDivisi' => $request->kepalaDivisi,
+            ]);
+
+            return redirect('/divisi/index');
+        } else return redirect('/login');
+    }
+
+    public function delete($noDivisi)
+    {
+        if (auth()->user()) {
+            DB::table('divisis')->where('noDivisi', $noDivisi)->delete();
+            return redirect('divisi/index');
+        }
+        else return redirect('/login');
+    }
 }
