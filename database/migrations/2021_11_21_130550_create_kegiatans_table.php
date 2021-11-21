a505bbfc7477aa2171cc4fdd6d0f727f3edaa8c7
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-21 21:05:35 +0700

    Update 2021_11_21_130550_create_kegiatans_table.php

--- database/migrations/2021_11_21_130550_create_kegiatans_table.php
+++ database/migrations/2021_11_21_130550_create_kegiatans_table.php
@@ -14,8 +14,17 @@ class CreateKegiatansTable extends Migration
     public function up()
     {
         Schema::create('kegiatans', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            // $table->bigIncrements('id');
+            $table->string('noKegiatan')->primary();
+            $table->string('divisi_id')->index();
+            $table->string('judulKegiatan');
+            $table->string('jenisKegiatan');
+            $table->date('jadwalKegiatan');
+            $table->string('tuk');
+
             $table->timestamps();
+
+            $table->foreign('divisi_id')->references('noDivisi')->on('divisis');
         });
     }
 
