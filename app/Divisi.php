5efe83c668ce2ebfc5a0fb9d9bed85180abeff14
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-21 20:08:22 +0700

    Update Divisi.php

--- app/Divisi.php
+++ app/Divisi.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Divisi extends Model
 {
     protected $guarded = [];
+
+    public function anggotas(){
+        return $this->hasMany(Anggota::class);
+    }
 }
