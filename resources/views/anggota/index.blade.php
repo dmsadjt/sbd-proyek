068bb128edd8a83eb6f2c70130f7283443ac14df
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-21 20:08:36 +0700

    Create index.blade.php

---
+++ resources/views/anggota/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card" style="width:100%">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table table-hover table-responsive">
+                        <thead>
+                          <tr>
+                            <th scope="col">NRP</th>
+                            <th scope="col">Nama Depan</th>
+                            <th scope="col">Nama Belakang</th>
+                            <th scope="col">Nama Panggilan</th>
+                            <th scope="col">Asal Jurusan</th>
+                            <th scope="col">Tempat, Tanggal Lahir</th>
+                            <th scope="col">Alamat</th>
+                            <th scope="col">Nomor HP</th>
+                            <th scope="col">Nomor Divisi</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($anggota as $a)
+                            <tr>
+                                <td>{{$a->NRP}}</td>
+                                <td>{{$a->namaDepan}}</td>
+                                <td>{{$a->namaBelakang}}</td>
+                                <td>{{$a->namaPanggilan}}</td>
+                                <td>{{$a->asalJurusan}}</td>
+                                <td>{{$a->ttl}}</td>
+                                <td>{{$a->alamat}}</td>
+                                <td>{{$a->noHP}}</td>
+                                <td>{{$a->divisi_id}}</td>
+                                <td><a href="/anggota/edit/{{$a->NRP}}">Update</a></td>
+                                <td><a href="/anggota/delete/{{$a->NRP}}">Hapus</a></td>
+                              </tr>
+                            @endforeach
+                        </tbody>
+                      </table>
+
+                      <a href="/anggota/add" class="btn btn-primary">Tambah</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
