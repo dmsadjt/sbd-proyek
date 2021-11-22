1fc6932bddc58c6a0538a13421da55c022f2a32f
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-22 22:08:38 +0700

    Create index.blade.php

---
+++ resources/views/jadwal/index.blade.php
@@ -0,0 +1,58 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">Nomor Jadwal</th>
+                            <th scope="col">Nomor Divisi</th>
+                            <th scope="col">Senin</th>
+                            <th scope="col">Selasa</th>
+                            <th scope="col">Rabu</th>
+                            <th scope="col">Kamis</th>
+                            <th scope="col">Jumat</th>
+                            <th scope="col">Sabtu</th>
+                            <th scope="col">Minggu</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($jadwal as $j)
+                            <tr>
+                                <td>{{$j->noJadwal}}</td>
+                                <td>{{$j->divisi_id}}</td>
+                                <td>{{$j->senin}}</td>
+                                <td>{{$j->selasa}}</td>
+                                <td>{{$j->rabu}}</td>
+                                <td>{{$j->kamis}}</td>
+                                <td>{{$j->jumat}}</td>
+                                <td>{{$j->sabtu}}</td>
+                                <td>{{$j->minggu}}</td>
+                                <td><a href="/jadwal/edit/{{$j->noJadwal}}">Update</a></td>
+                                <td><a href="/jadwal/delete/{{$j->noJadwal}}">Hapus</a></td>
+                              </tr>
+                            @endforeach
+                        </tbody>
+                      </table>
+
+                      <a href="/jadwal/add" class="btn btn-primary">Tambah</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
