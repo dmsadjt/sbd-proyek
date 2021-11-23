d234ec51ad980bc078c7503c6a95c6a3428fc0dd
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-23 18:25:15 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/role';
 
     /**
      * Create a new controller instance.
