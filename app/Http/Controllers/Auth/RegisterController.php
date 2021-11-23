0c93b590cca6f824a0d874a343e383fef7667f5a
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-23 18:25:13 +0700

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/role';
 
     /**
      * Create a new controller instance.
