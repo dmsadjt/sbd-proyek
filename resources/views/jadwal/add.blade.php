439e1b4b9f7ab58089c079352bdb59ca29c5e20d
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-25 12:00:17 +0700

    Update add.blade.php

--- resources/views/jadwal/add.blade.php
+++ resources/views/jadwal/add.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">Tambah Kegiatan</div>
+                <div class="card-header">Tambah Jadwal</div>
 
                 <div class="card-body">
                     <form method="post" action="/jadwal/store">
