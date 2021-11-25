f9047eb3a8e70a571dac6713bebbb8281e17dc5c
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-25 12:00:19 +0700

    Update edit.blade.php

--- resources/views/jadwal/edit.blade.php
+++ resources/views/jadwal/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Tambah Divisi</div>
+                <div class="card-header">Edit Jadwal</div>
                 <div class="card-body">
                     <form method="post" action="/jadwal/update">
                         @csrf
