a5e003ee37ddc85f54a95c2d663b5eedd320155b
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-29 10:35:08 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,19 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/role';
+    //protected $redirectTo = '/role';
+
+    protected function redirectTo( ) {
+        if (Auth::check() && Auth::user()->id == 3) {
+            return('/home/anggota');
+        }
+        elseif (Auth::check() && Auth::user()->id == 2) {
+            return('/home/kadiv');
+        }
+        else {
+            return('/home/ksb');
+        }
+    }
 
     /**
      * Create a new controller instance.
