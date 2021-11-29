59fa6d5686c3a4684a8020ba6280db5484a606aa
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-29 10:35:21 +0700

    Create Kadiv.php

---
+++ app/Http/Middleware/Kadiv.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class Kadiv
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    function handle($request, Closure $next)
+    {
+      if (Auth::check() && Auth::user()->id == 2) {
+          return $next($request);
+      }
+      elseif (Auth::check() && Auth::user()->id == 3) {
+          return redirect('/home/kadiv');
+      }
+      else {
+          return redirect('/home/ksb');
+      }
+    }
+}
