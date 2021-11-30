784ded08bf2c5fec25d66f237a31334a7eba8757
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2021-11-30 22:11:06 +0700

    Update edit.blade.php

--- resources/views/kegiatan/edit.blade.php
+++ resources/views/kegiatan/edit.blade.php
@@ -65,7 +65,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="jadwalKegiatan" class="col-md-4 col-form-label text-md-right">Judul Kegiatan</label>
+                            <label for="jadwalKegiatan" class="col-md-4 col-form-label text-md-right">Jadwal Kegiatan</label>
 
                             <div class="col-md-6">
                                 <input id="jadwalKegiatan" type="date"
